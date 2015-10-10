f4c7fc29ef57e43a9efab86180ec9b0f922cac69
Sridhara Dasu <dasu@hep.wisc.edu>
2015-10-10 23:49:56 +0200

    Renamed process

--- test/testL1TCaloLayer1Spy.py
+++ test/testL1TCaloLayer1Spy.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("L1TCaloLayer1Test")
+process = cms.Process("L1TCaloLayer1Spy")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('L1Trigger.L1TCaloLayer1Spy.l1tCaloLayer1SpyDigis_cfi')
@@ -17,9 +17,6 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('l1tCaloLayer1Spy.root'),
     outputCommands = cms.untracked.vstring('keep *')
 )
-
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = '74X_dataRun2_Express_v1'
 
 process.p = cms.Path(process.l1tCaloLayer1SpyDigis)
 
