7f00fa5d77e302cdd89c7dbff507bf96ca58d75a
Sridhara Dasu <dasu@hep.wisc.edu>
2015-11-02 19:33:31 +0100

    Added argument to setupString for the Spy

--- test/testL1TCaloLayer1Spy.py
+++ test/testL1TCaloLayer1Spy.py
@@ -1,11 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing()
+options.register('setupString', "captures:/data/dasu/Layer1ZeroBiasCaptureData/r260490_1", VarParsing.multiplicity.singleton, VarParsing.varType.string, 'L1TCaloLayer1Spy setupString')
+options.parseArguments()
+
 process = cms.Process("L1TCaloLayer1Spy")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load('L1Trigger.L1TCaloLayer1Spy.l1tCaloLayer1SpyDigis_cfi')
 
-process.l1tCaloLayer1SpyDigis.setupString = cms.untracked.string("captures:/data/dasu/Layer1ZeroBiasCaptureData/r260490_1")
+process.load('L1Trigger.L1TCaloLayer1Spy.l1tCaloLayer1SpyDigis_cfi')
+process.l1tCaloLayer1SpyDigis.setupString = cms.untracked.string(options.setupString)
 
 # Put multiples of 162 - output data for eighteen BXs are available for each capture
 # One event is created for each capture.  Putting non-multiples of 162 just means
